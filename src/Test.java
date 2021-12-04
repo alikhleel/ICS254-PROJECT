2889b0188c332b5bb7ea036432c3a857538c7c34
Ali Khalil <ali.moan9@gmail.com>
2021-12-04 19:53:27 +0300

    Revert "Delete src\Test.java"
    This reverts commit 304c8012b1515a9efe3e002ee96f7759ad6f8edf.

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) {
+        EncryptFileRSA fileRSA = new EncryptFileRSA(new File("input.txt"));
+        fileRSA.encrypt();
+
+        long n = 0;
+        long d = 0;
+        DecryptFileRSA.decrypt(new File("input.rsa"), n, d);
+
+
+    }
+}
