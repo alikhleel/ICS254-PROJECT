5d3aa90d60a7ff0689401f78552e4f71291522ae
Ali Khalil <ali.moan9@gmail.com>
2021-12-07 20:48:48 +0300

    Modify  EncryptFileRSA

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,9 @@
-import java.io.File;
 import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        FilesDeal filesDeal = new FilesDeal("try.txt");
-        System.out.println(filesDeal.hasBlock(2));
-        System.out.println(filesDeal.getBlock());
+        EncryptFileRSA encryptFileRSA = new EncryptFileRSA("try.txt");
+        encryptFileRSA.encrypt();
+      // test decryption
     }
 }
